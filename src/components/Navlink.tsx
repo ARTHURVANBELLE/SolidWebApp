1079d13ecccf92130fa78649189899a82d4e9161
ARTHURVANBELLE <23319@ecam.be>
2025-02-12 11:47:07 +0100

    Input on the add-activity tab

--- src/components/Navlink.tsx
+++ src/components/Navlink.tsx
@@ -1,20 +1,20 @@
 import { JSXElement } from "solid-js";
 import { useLocation } from "@solidjs/router";
 
-
 type NavLinkProps = {
-    href : string;
-    children : JSXElement;
-}
+  href: string;
+  children: JSXElement;
+};
 
-export
-function NavLink(props: NavLinkProps) {
-    const location = useLocation();
-    const active = (path: string) =>
-        path == location.pathname ? "border-sky-600" : "border-transparent hover:border-sky-600";
-    return (
-        <li class={`border-b-2 ${active(props.href)} mx-1.5 sm:mx-6`}>
-            <a href={props.href}>{props.children}</a>
-        </li>
-    );
+export function NavLink(props: NavLinkProps) {
+  const location = useLocation();
+  const active = (path: string) =>
+    path == location.pathname
+      ? "border-sky-600"
+      : "border-transparent hover:border-sky-600";
+  return (
+    <li class={`border-b-2 ${active(props.href)} mx-1.5 sm:mx-6`}>
+      <a href={props.href}>{props.children}</a>
+    </li>
+  );
 }
--- src/routes/new-activity.tsx
+++ src/routes/new-activity.tsx
@@ -1,25 +1,43 @@
 import { A } from "@solidjs/router";
 import Counter from "~/components/Counter";
+import { TextInput } from "~/components/TextInput";
+import { createSignal } from "solid-js";
 
-export default function About() {
+export default function NewActivity() {
+  const [title, setTitle] = createSignal("");
+  const [error, setError] = createSignal("");
+
+  const handleInput = (event: InputEvent) => {
+    setTitle((event.target as HTMLInputElement).value);
+    if ((event.target as HTMLInputElement).value.length < 3) {
+      setError("Title must be at least 3 characters long");
+    } else {
+      setError("");
+    }
+  };
+
+  let inputRef: HTMLInputElement | undefined;
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">New Activity Page</h1>
-      <Counter />
-      <p class="mt-8">
-        Visit{" "}
-        <a href="https://solidjs.com" target="_blank" class="text-sky-600 hover:underline">
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <A href="/" class="text-sky-600 hover:underline">
-          Home
-        </A>
-        {" - "}
-        <span>About Page</span>
-      </p>
+      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Create a New Activity</h1>
+      <form onSubmit={(e) => e.preventDefault()}>
+      <TextInput
+        name="username"
+        type="text"
+        label="Your Name"
+        placeholder="Enter your name"
+        value={title()}
+        error={error()}
+        required={true}
+        ref={(el) => (inputRef = el)}
+        onInput={handleInput}
+        onChange={() => {}}
+        onBlur={() => {}}
+      />
+      <button type="submit" disabled={!!error()}>
+        Submit
+      </button>
+    </form>
     </main>
   );
 }

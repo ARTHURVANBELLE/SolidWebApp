85b2d861325b7864796f7e9784197e6fa440b6fd
ARTHURVANBELLE <23319@ecam.be>
2025-02-12 10:49:49 +0100

    NavBar contains basic pages

---
+++ src/components/Nav.tsx
@@ -0,0 +1,22 @@
+import { useLocation } from "@solidjs/router";
+import {NavLink} from "./Navlink";
+
+export default function Nav() {
+  const location = useLocation();
+  const active = (path: string) =>
+    path == location.pathname ? "border-sky-600" : "border-transparent hover:border-sky-600";
+  return (
+    <nav class="flex justify-between items-center w-full p-4">
+      <ul class="flex">
+        <NavLink href="/">Home</NavLink>
+        <NavLink href="/new-activity">New Activity</NavLink>
+        <NavLink href="/about">About</NavLink>
+      </ul>
+
+      <ul class="flex">
+        <NavLink href="/admin">Admin</NavLink>
+      </ul>
+    </nav>
+  );
+}
+

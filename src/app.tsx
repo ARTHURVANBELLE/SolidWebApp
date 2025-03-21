f08141f244b5f8d4ca96a30fa24f26b001629bd4
ARTHURVANBELLE <23319@ecam.be>
2025-03-21 16:47:08 +0100

    managed to make the project run again but the Strava Authentication needs fix with the API POST Request

--- src/app.tsx
+++ src/app.tsx
@@ -6,19 +6,41 @@ import ErrorBoundary from "./components/ErrorBoundary";
 import Debug from "./components/Debug";
 import { getLoginUrl } from "./utils/session";
 
-export default function Root() {
+export default function App() {
   return (
     <Router>
-      <ErrorBoundary>
         <Suspense fallback={<p>Loading...</p>}>
           {import.meta.env.DEV && <Debug />}
         </Suspense>
-      </ErrorBoundary>
+    </Router>
+  );
+}
+
+*/
+import { Router } from "@solidjs/router";
+import { FileRoutes } from "@solidjs/start/router";
+import { Suspense } from "solid-js";
+import Nav from "~/components/Nav";
+import "./app.css";
+
+export default function App() {
+  return (
+    <Router
+      root={props => (
+        <>
+          <Nav />
+          <Suspense>{props.children}</Suspense>
+        </>
+      )}
+    >
+      <FileRoutes />
     </Router>
   );
 }
 
 
+
+/*
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
@@ -38,7 +60,7 @@ export default function App() {
   );
 }
 
-*/
+
 
 import { Router } from "@solidjs/router";
 //import { FileRoutes } from "@solidjs/start";
@@ -58,4 +80,4 @@ export default function App() {
   );
 }
 
-
+*/

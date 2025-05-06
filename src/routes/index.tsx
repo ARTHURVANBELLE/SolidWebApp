2ca7c7b5d2737a0ba42fb60a04a2e2b93866f99d
Arthur <23319@ecam.be>
2025-05-06 15:30:10 +0200

    light cleanup of unused component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,7 @@
-import { A } from "@solidjs/router";
-import Counter from "~/components/Counter";
 import Layout from "~/components/Layout";
 import {getUser, getSessionData} from "../utils/session";
-import SessionDebug from "~/components/SessionDebug";
 import { createAsync } from "@solidjs/router";
-import StravaActivities from "~/components/StravaActivities";
+import pelotonImage from "~/assets/image/large_peloton.jpg";
 
 export default function Home() {
     const user = createAsync(() => getUser());
@@ -16,10 +13,16 @@ export default function Home() {
   return (
     <Layout protected={true}>
       <main class="text-center mx-auto text-gray-700 p-4">
-        <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-          Hello world!
+        <div class="w-full mb-8">
+          <img 
+            src={pelotonImage} 
+            alt="Large peloton cycling image" 
+            class="w-full object-cover max-h-[250px]" 
+          />
+        </div>
+        <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16 justify-baseline">
+          Coming events
         </h1>
-        <Counter />
         <p class="mt-8">
         </p>
       </main>
